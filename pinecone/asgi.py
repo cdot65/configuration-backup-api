51539655e89639665e4867f9b600a20628b9d2b1
cremsburg <Calvin Remsburg>
2021-03-08 12:08:04 +0000

    rebranding for market love

--- pinecone/asgi.py
+++ pinecone/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for dcim project.
+ASGI config for pinecone project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dcim.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pinecone.settings')
 
 application = get_asgi_application()
--- pinecone/urls.py
+++ pinecone/urls.py
@@ -1,4 +1,4 @@
-"""dcim URL Configuration
+"""pinecone URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
